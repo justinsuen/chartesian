e244afe9af0246a80bacabeb28cf370c1003daec
Justin Suen <jsuen27@gmail.com>
2017-01-10 15:24:39 -0800

    Complete bootstrapping and reset CSS

--- frontend/chartesian.jsx
+++ frontend/chartesian.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
 import Root from './components/root';
-import { login, logout, signup } from './actions/session_actions';
-
-window.login = login;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  const store = configureStore();
-  window.store = store;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
 
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
 });
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -8,15 +8,31 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
-const Root = ({ store }) => (
-  <Provider store={store}>
+const Root = ({ store }) => {
+  const _ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/login');
+    }
+  };
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return(
+    <Provider store={store}>
       <Router history={hashHistory}>
         <Route path="/" component={App}>
-          <Route path="/login" component={ SessionFormContainer } />
-          <Route path="/signup" component={ SessionFormContainer } />
+          <Route path="/login" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn}/>
+          <Route path="/signup" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn}/>
         </Route>
       </Router>
-  </Provider>
-);
+    </Provider>
+  );
+};
 
 export default Root;
