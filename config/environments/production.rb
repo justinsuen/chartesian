13ac0cf2870aa0ab0edcfeb85c4b9e08dc44e2a1
Justin Suen <jsuen27@gmail.com>
2017-01-11 00:39:33 -0800

    Update production.rb for assets on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.serve_static_files = true
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
