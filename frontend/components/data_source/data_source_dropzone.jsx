240d0670d77ca0feaa2a6c6472111445af1a42f5
Justin Suen <jsuen27@gmail.com>
2017-01-12 15:26:45 -0800

    Initialize data source component

---
+++ frontend/components/data_source/data_source_dropzone.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DataSourceDropzone extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        // your code here...
+      </div>
+    )
+  }
+}
+
+export default DataSourceDropzone;
