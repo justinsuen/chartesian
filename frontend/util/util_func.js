7535ee44ccea1865d73bdcffc54f0c1c28acc2e7
Justin Suen <jsuen27@gmail.com>
2017-01-13 15:27:24 -0800

    Work on file parser

--- frontend/util/util_func.js
+++ frontend/util/util_func.js
@@ -1,9 +1,29 @@
 export const genRandNum = n => Math.ceil(Math.random() * n);
 
-export const toTitleCase = (str) => {
+export const toTitleCase = str => {
   let upStr = str.replace(/\w\S*/g,
     function(txt) {
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
   return upStr.replace(/-/g, " ");
+};
+
+export const parseCSV = (file, row, col) => {
+  const reader = new FileReader();
+
+  const fileLines = file.split(/\r\n|\n/);
+  const headers = fileLines[0].split(',');
+  let lines = [];
+
+  for (let i = 1; i < fileLines.length; i++) {
+    let data = fileLines[i].split(',');
+    if (data.length === headers.length) {
+
+      let arr = [];
+      for (let j = 0; j < headers.length; j++) {
+        arr.push(headers[j] + ":" + data[j]);
+      }
+      lines.push(arr);
+    }
+  }
 };
