8316e2a702ec8fb848f1acd46800c9d3ac5ef90e
Justin Suen <jsuen27@gmail.com>
2017-01-20 15:24:09 -0800

    Not finishing share before push

--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -14,7 +14,7 @@ class Sidebar extends React.Component {
   }
 
   render() {
-    const types = ["data-source", "chart", "share"];
+    const types = ["data-source", "chart"];
     if (this.props.currentUser) {
       return (
         <div className="sidebar">
