acd4584a4b21d5a0104cab3e755063cc705ec205
Justin Suen <jsuen27@gmail.com>
2017-01-20 12:21:47 -0800

    Seed with the KOBE

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,43 +42,23 @@ def json_beautify(data)
   json
 end
 
-employees_raw = dsv_to_json(parse_tsv(File.read('./app/assets/seed_data_source/employees.tsv')))
-bad_drivers = dsv_to_json(CSV.parse(File.read('./app/assets/seed_data_source/bad_drivers.csv')))
-recent_grads = dsv_to_json(CSV.parse(File.read('./app/assets/seed_data_source/recent_grads.csv')))
+kobe_stats = dsv_to_json(parse_tsv(File.read('./app/assets/seed_data_source/kobe_stats.csv')))
 us_pop_2010 = json_beautify(JSON.parse(File.read('./app/assets/seed_data_source/us_pop_2010.json')))
 swe_pop_2015 = json_beautify(JSON.parse(File.read('./app/assets/seed_data_source/swe_pop_2015.json')))
+recent_grads = dsv_to_json(CSV.parse(File.read('./app/assets/seed_data_source/recent_grads.csv')))
 china_gdp = json_beautify(JSON.parse(File.read('./app/assets/seed_data_source/china_gdp.json')))
 usd_xrate = json_beautify(JSON.parse(File.read('./app/assets/seed_data_source/20170119_usd.json')))
+employees_raw = dsv_to_json(parse_tsv(File.read('./app/assets/seed_data_source/employees.tsv')))
+bad_drivers = dsv_to_json(CSV.parse(File.read('./app/assets/seed_data_source/bad_drivers.csv')))
 
 # Create data sources
 (1..99).each do |num|
   DataSource.create!(
-    title: 'Recent Grads',
-    data_type: 'csv',
-    owner_id: num,
-    data_source_url: 'chartesian_demo_grads_url',
-    table: recent_grads
-  )
-  DataSource.create!(
-    title: 'ABC Company Employees',
-    data_type: 'tsv',
-    owner_id: num,
-    data_source_url: 'chartesian_demo_employees_url',
-    table: employees_raw
-  )
-  DataSource.create!(
-    title: 'US Driving Statistics',
-    data_type: 'csv',
-    owner_id: num,
-    data_source_url: 'chartesian_demo_drivers_url',
-    table: bad_drivers
-  )
-  DataSource.create!(
-    title: 'China GDP 1960-2015',
-    data_type: 'tsv',
-    owner_id: num,
-    data_source_url: 'chartesian_demo_china_gdp_url',
-    table: china_gdp
+  title: 'Kobe Bryant Career Statistics',
+  data_type: 'csv',
+  owner_id: num,
+  data_source_url: 'chartesian_demo_kobe_url',
+  table: kobe_stats
   )
   DataSource.create!(
     title: '2010 US Population',
@@ -95,10 +75,38 @@ usd_xrate = json_beautify(JSON.parse(File.read('./app/assets/seed_data_source/20
     table: swe_pop_2015
   )
   DataSource.create!(
+  title: 'Recent Grads',
+  data_type: 'csv',
+  owner_id: num,
+  data_source_url: 'chartesian_demo_grads_url',
+  table: recent_grads
+  )
+  DataSource.create!(
+  title: 'China GDP 1960-2015',
+  data_type: 'tsv',
+  owner_id: num,
+  data_source_url: 'chartesian_demo_china_gdp_url',
+  table: china_gdp
+  )
+  DataSource.create!(
     title: 'USD Exchange Rate (2017/01/19)',
     data_type: 'json',
     owner_id: num,
     data_source_url: 'chartesian_demo_20170119_usd_url',
     table: usd_xrate
+  )
+  DataSource.create!(
+  title: 'ABC Company Employees',
+  data_type: 'tsv',
+  owner_id: num,
+  data_source_url: 'chartesian_demo_employees_url',
+  table: employees_raw
+  )
+  DataSource.create!(
+    title: 'US Driving Statistics',
+    data_type: 'csv',
+    owner_id: num,
+    data_source_url: 'chartesian_demo_drivers_url',
+    table: bad_drivers
   )
 end
