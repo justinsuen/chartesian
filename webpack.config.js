53ef5c48ed76d31184ff0df4dd01041d57b7b21d
Justin Suen <jsuen27@gmail.com>
2017-01-20 22:14:53 -0800

    Use webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './frontend/chartesian.jsx',
   output: {
@@ -5,23 +7,28 @@ module.exports = {
     filename: 'bundle.js',
   },
   module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /\.png$/,
-        loader: "url-loader?mimetype=image/png"
+    loaders: [{
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
       }
+    }, {
+      test: /\.png$/,
+      loader: "url-loader?mimetype=image/png"
+    }],
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin()
     ]
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['', '.js', '.jsx' ]
+    extensions: ['', '.js', '.jsx']
   }
 };
