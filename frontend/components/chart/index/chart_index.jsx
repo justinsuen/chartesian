8d5c08ce6e65ca99430a2996343a98eac6c546b8
Justin Suen <jsuen27@gmail.com>
2017-02-23 13:14:19 -0800

    Make chart index and share index nicer

--- frontend/components/chart/index/chart_index.jsx
+++ frontend/components/chart/index/chart_index.jsx
@@ -17,15 +17,29 @@ class ChartIndex extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div className="chart-index-container">
-        <h2>Manage Charts</h2>
+  renderCharts() {
+    if (this.props.charts.length !== 0) {
+      return (
         <div className="chart-items-container">
           {this.props.charts.map((chart, idx) =>
             <ChartItem key={idx} chart={chart}/>
           )}
         </div>
+      );
+    } else {
+      return (
+        <div className="chart-items-container no-charts">
+          <h2>You haven't made a chart yet!</h2>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="chart-index-container">
+        <h2>Manage Charts</h2>
+        { this.renderCharts() }
         <div className="footer">
           <p>Chart sprites created by <a href="http://www.flaticon.com/authors/maxim-basinski">Maxim Basinski</a></p>
         </div>
--- frontend/components/share/index/share_index.jsx
+++ frontend/components/share/index/share_index.jsx
@@ -11,15 +11,29 @@ class ShareIndex extends React.Component {
     this.props.fetchInCharts();
   }
 
-  render() {
-    return (
-      <div className="share-index-container">
-        <h2>Resources Shared with You</h2>
+  renderCharts() {
+    if (this.props.charts.length !== 0) {
+      return (
         <div className="chart-items-container">
           {this.props.charts.map((chart, idx) =>
             <ChartItem key={idx} chart={chart}/>
           )}
         </div>
+      );
+    } else {
+      return (
+        <div className="chart-items-container no-charts">
+          <h2>Nobody shared anything with you yet!</h2>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="share-index-container">
+        <h2>Shared with Me</h2>
+        { this.renderCharts() }
         <div className="footer">
           <p>Chart sprites created by <a href="http://www.flaticon.com/authors/maxim-basinski">Maxim Basinski</a></p>
         </div>
